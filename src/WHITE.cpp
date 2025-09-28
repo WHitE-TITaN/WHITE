51f9f8dad0354854dc1a6e26a8f4956a35762988
WHitE-TITaN <dk488621@gmail.com>
2025-09-28 15:37:36 -0400

    tested program execution for sandboxing and argument execution

--- src/WHITE.cpp
+++ src/WHITE.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <string>
 #include "os.h"
+#include "nameSpace.h"
 
 int main(){
   getOs *crOS = currentOS();
   std::cout << "running - > Current OS: " << crOS->os << std::endl;
   delete crOS; // Clean up allocated memory
+
+  sandBox *process;
+  std::string path = "/home/kali/Documents/GitHub/WHITE/TestSubject1";
+  process->runItBoxed(&path);
   return 0;
 }
+
