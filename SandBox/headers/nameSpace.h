e608e437456d92b9188f27ff5a2cb369d7d8aff1
WHitE-TITaN <dk488621@gmail.com>
2025-09-29 16:07:17 -0400

    updated main binarty for attaching the logger to the child process

--- SandBox/headers/nameSpace.h
+++ SandBox/headers/nameSpace.h
@@ -5,49 +5,81 @@
 #include <string.h>     // for strerror
 #include <stdlib.h>     // for exit
 #include <iostream>
+#include <sys/wait.h>
 
 class sandBox{
-    //memory for sandBox.
-    #define STACK_SIZE (1024 * 1024); //1024byte = 1 kb -> 1024 * 1024 = 1048576byte = 1024kb = 1MB.
-
     public:
-    int createNameSpace(std::string* path);
-    int runItBoxed(std::string* path);
-    int runItBoxed(std::string* path, std::string* args);
+        //to execute program in sandBox 
+        static int runItBoxed(void* args){
+            //getpid() will featch the process id that is being executed.
+            std::cout<<"pid - > "<<getpid();
+            std::cout.flush(); //send print buffer to the terminal immidatly as execel will replace the
+            //current execution flow.
+
+            //convert arg to valid string path as execl required a char* 
+            const char * path = static_cast<const char*>(args);
+
+            //execl is used for executing a program
+            //and return -1 if program is fail to run
+            //perror() tells the error why the program failed.
+            int returns = execl(path, path, NULL);
+            perror("execl");
+
+            return 1;
+        }
+
+        //to create SandBox
+        int createNameSpace(const char* path){
+            //allocating stack.
+            #define STACK_SIZE (1024 * 1024) //1024byte = 1 kb -> 1024 * 1024 = 1048576byte = 1024kb = 1MB.
+            void* child_stack = malloc(STACK_SIZE);
+
+            //CLONE_NEWPID create a new pi for child and sandbox it so it cant see any other process
+            //but a real PID is also attached that is visible to Parent process
+            //SIGCHILD = is a flag that let Parent recive signal form child Process.
+            int flag = CLONE_NEWPID | SIGCHLD;
+
+            //pid_t is a signed integer that represent process id in linux and macos
+            //clone() returns a real pid for the child process but due to CLONE_NEWPID flag child process only sees it self.
+            pid_t child = clone(runItBoxed, (char*) child_stack + STACK_SIZE, flag, (void*)path);
+            std::cout<<"child pid -> "<<child;
+
+            if(child == -1){
+                perror("clone");
+                exit(EXIT_FAILURE);
+            }
+
+            //attach the logger to log all the activity the child do -> bpftrace
+            //track the hole system.
+            // int tacker = execl("/bin/bash", "/bin/bash", "-c", 
+            //     "sudo bpftrace -e \"tracepoint:syscalls:sys_enter_openat { "
+            //     "printf(\\\"%s opened %s\\n\\\", comm, str(args->filename)); }\"",
+            //     NULL);
+
+            //track specific PID
+            std::string trackCMD =  
+                "sudo bpftrace -e \"tracepoint:syscalls:sys_enter_openat /pid == " + std::to_string(child) + "/ { "
+                "printf(\\\"%s opened %s\\n\\\", comm, str(args->filename)); }\"";
+            //execute the track command on new bash
+            int tacker = execl("/bin/bash", "/bin/bash", "-c",
+                trackCMD.c_str(),
+                NULL);
+
+            perror("execl");
+
+            //to wait until the executioon of child is finished
+            waitpid(child, NULL, 0);
+            return 1;
+        }
+
+    private:
+        void trackSystem(){
+            //create a subprocess that log every process detail.
+            int tacker = execl("/bin/bash", "/bin/bash", "-c", 
+                "sudo bpftrace -e \"tracepoint:syscalls:sys_enter_openat { "
+                "printf(\\\"%s opened %s\\n\\\", comm, str(args->filename)); }\"",
+                NULL);
+
+            perror("execl");
+        }
 };
-
-
-//to create SandBox
-int sandBox::createNameSpace(std::string* path){
-    return 1;
-}
-
-
-//to execute program in sandBox 
-int sandBox::runItBoxed(std::string* path){
-    //getpid() will featch the process id that is being executed.
-    std::cout<<"pid - > "<<getpid();
-    std::cout.flush(); //send print buffer to the terminal immidatly as execel will replace the
-    //current execution flow. 
-
-    
-    const char * programPath = path->c_str();
-
-    //execl is used for executing a program
-    //and return -1 if program is fail to run
-    //perror() tells the error why the program failed.
-    int returns = execl(programPath, programPath, NULL);
-    if(returns == -1)perror("execl");
-
-    return 1;
-}
-
-
-//sand box with arguments
-int sandBox::runItBoxed(std::string *path, std::string* args){
-    int returns = execl(path->c_str(), path->c_str(), "-c", args->c_str(), NULL);
-    if(returns == -1) perror("execl");
-    return 1;
-}
-
-
--- src/WHITE.cpp
+++ src/WHITE.cpp
@@ -1,16 +1,89 @@
 #include <iostream>
 #include <string>
 #include "os.h"
+#include <limits.h>
 #include "nameSpace.h"
 
+std::string getPath(){
+  //defined max path length
+   char buffer[PATH_MAX];
+   //read what is the location of the binnary or where it is being executed.
+    ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+    if (len == -1) {
+        perror("readlink");
+        exit(1);
+    }
+    //read buffer and return path
+    buffer[len] = '\0';
+    std::string fullPath(buffer);
+    return fullPath.substr(0, fullPath.find_last_of('/'));
+}
+
+//installing prequisite before execution of sandBox.
+void installPreRequisite(){
+  //installing prerequisites
+  //fork to copy pid from parentproces
+  // > 0 goes to child
+  // = 0 its the child
+  // < 0 failed
+  pid_t child = fork();
+  if(child == -1){
+    perror("fork");
+    exit(1);
+  }
+  //if child process instal the dependecy.
+  if(child == 0){
+    std::string preRequisite = getPath()+"/PreRequisite";
+    std::cout<<"\n"+preRequisite+"\n";
+    int installing = execl(preRequisite.c_str(), preRequisite.c_str(), NULL);
+    //if crash or fails log issue.
+    perror("execl");
+  }
+
+  else{
+    //saves the state the program currently is.
+    int status;
+    //wait for the parent to be exercuted.
+    if (waitpid(child, &status, 0) == -1) {
+      perror("waitpid failed");
+      exit(1);
+    }
+    //if the parent have exited before executing the child
+    if(WIFEXITED(status)){
+      //check for the exeit code if killed or some other issue.
+      int exitCode = WEXITSTATUS(status);
+      if (exitCode != 0) {
+        std::cerr << "Prerequisite binary exited with error: " << exitCode << "\n";
+        exit(1);
+      }
+    
+    //uif the program crash with some error.
+    } else {
+      std::cerr << "Prerequisite binary crashed or was killed\n";
+      exit(1);
+    }
+  }
+}
+
+
+
+
+
+
+//main code flow ->
 int main(){
+  //getting the current os version
   getOs *crOS = currentOS();
   std::cout << "running - > Current OS: " << crOS->os << std::endl;
   delete crOS; // Clean up allocated memory
 
-  sandBox *process;
+  //install dependency
+  installPreRequisite();
+  std::cout.flush();
+
+  sandBox process;
   std::string path = "/home/kali/Documents/GitHub/WHITE/TestSubject1";
-  process->runItBoxed(&path);
+  process.createNameSpace(path.c_str());
   return 0;
 }
 
