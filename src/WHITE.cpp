a907c3143e2031e57a6164ff89704c023599b32b
WHitE-TITaN <dk488621@gmail.com>
2025-10-06 14:28:02 -0400

    add: dynamic boot Generation

--- src/WHITE.cpp
+++ src/WHITE.cpp
@@ -4,6 +4,15 @@
 #include <limits.h>
 #include "nameSpace.h"
 
+//for system calls
+#include <cstdlib>
+//to check pth
+#include <filesystem>
+
+//to sorten the std::filesystem -> fs
+namespace fs = std::filesystem;
+
+
 std::string getPath(){
   //defined max path length
    char buffer[PATH_MAX];
@@ -18,6 +27,49 @@ std::string getPath(){
     std::string fullPath(buffer);
     return fullPath.substr(0, fullPath.find_last_of('/'));
 }
+
+
+
+//checking files and previous root builds
+bool setupDirectoryAndDebootstrap(const fs::path& dir){
+  //check if the directory exsist
+  if(fs::is_directory(dir)){
+    std::cout<<"\nDirectory exsist : "<<dir;
+    return true;
+  }
+
+  std::cout<<"\nDirectory not found \nBuilding Boot";
+  try{
+    //create_directories() creates directory in correct order.
+    fs::create_directories(dir);
+    std::cout<<"\nDirectory building completed : "<<dir;
+  }
+  catch(fs::filesystem_error& e){
+    //print error if fail to build dir.
+    std::cout<<"\nError : please reffer\n"<<e.what();
+    //rasing exception for main higher function to handle.
+    throw std::runtime_error("Fail to build Dir check setupDirectoryAndDebootstrap() : WHITE.cpp");
+  }
+
+  //building command for bootstrap executaion.
+  std::cout<<"\nRunning BootStrap :";
+  std::string bootStrapStart = "sudo debootstrap stable " + dir.string() + " http://deb.debian.org/debian/";
+
+  //running sysrtem command
+  int returnCode = system(bootStrapStart.c_str());
+  //processing results.
+  if(returnCode == 0){
+    std::cout<<"\nBootStrap Build Successfull";
+    return true;
+  }
+
+  std::cerr<<"\nBootStrap Build Fail";
+  //rasing exception for higher function ro handle.
+  throw std::runtime_error("BootStrap Fails check setupDirectoryAndDebootstrap() : WHITE.cpp");
+}
+
+
+
 
 //installing prequisite before execution of sandBox.
 void installPreRequisite(){
@@ -69,7 +121,6 @@ void installPreRequisite(){
 
 
 
-
 //main code flow ->
 int main(){
   //getting the current os version
@@ -81,8 +132,19 @@ int main(){
   installPreRequisite();
   std::cout.flush();
 
+  //Boot Check
+  fs::path bootDirectory = fs::path(getPath())/"mountMask";
+  try{
+    setupDirectoryAndDebootstrap(bootDirectory);
+  }
+  //if fail to build exit program.
+  catch(std::exception& e){
+    std::cerr<<"\nError ! reffer : "<<e.what();
+    exit(-1);
+  }
+
   sandBox process;
-  std::string path = "/home/kali/Documents/GitHub/WHITE/TestSubject1";
+  std::string path = "/bin/bash";
   process.createNameSpace(path.c_str());
   return 0;
 }
